d9e9e6e1e21c908a64690e79bf914fd540360281
idsvnasia <74672963+idsvnasia@users.noreply.github.com>
2021-08-08 23:59:42 +0700

    Update buildimage.sh

--- complete/buildimage.sh
+++ complete/buildimage.sh
@@ -5,18 +5,18 @@ BUILD_NUMBER=$((BULD_NUMBER+1))
 
 TAG_NAME=$BUILD_NUMBER
 
-./gradlew bootBuildImage --imageName=springio/gs-spring-boot-docker:v$TAG_NAME
+./gradlew bootBuildImage --imageName=springio/gs-spring-boot-docker:$TAG_NAME
 
 
-./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=springio/gs-spring-boot-docker:v$TAG_NAME
-
-
-
-docker run -e "SPRING_PROFILES_ACTIVE=dev" -p 8080:8080 -t springio/gs-spring-boot-docker:v$TAG_NAME
+./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=springio/gs-spring-boot-docker:$TAG_NAME
 
 
 
-docker run -p 8080:8080 -t springio/gs-spring-boot-docker:v${TAG_NAME}
+docker run -e "SPRING_PROFILES_ACTIVE=dev" -p 8080:8080 -t springio/gs-spring-boot-docker:$TAG_NAME
+
+
+
+docker run -p 8080:8080 -t springio/gs-spring-boot-docker:${TAG_NAME}
 
 
 curl http://localhost:8080/
