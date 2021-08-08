5803dbe792ed0af382462ddd3b9c7f0f09ab625d
idsvnasia <74672963+idsvnasia@users.noreply.github.com>
2021-08-08 23:24:23 +0700

    Update buildimage.sh

--- complete/buildimage.sh
+++ complete/buildimage.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 # Maven build & Docker build for java spring boot
 
-
-
-./mvnw package 
-
-
-JAR_FILE=target/*.jar
-
-
-cp $JAR_FILE  app.jar
-
-
-java -jar   /java -jar   
-
-
-curl http://localhost:8080/
-
-
-
-docker build -t springio/gs-spring-boot-docker:v1   .
+TAG_NAME=$1
 
 
 ./gradlew bootBuildImage --imageName=springio/gs-spring-boot-docker:v2
 
 
-./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=springio/gs-spring-boot-docker:v3
+./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=springio/gs-spring-boot-docker:${TAG_NAME}
 
 
 
-docker run -e "SPRING_PROFILES_ACTIVE=dev" -p 8080:8080 -t springio/gs-spring-boot-docker:v4
+docker run -e "SPRING_PROFILES_ACTIVE=dev" -p 8080:8080 -t springio/gs-spring-boot-docker:${TAG_NAME}
 
 
 
-docker run -p 8080:8080 -t springio/gs-spring-boot-docker:v4
+docker run -p 8080:8080 -t springio/gs-spring-boot-docker:${TAG_NAME}
+
+
+curl http://localhost:8080/
