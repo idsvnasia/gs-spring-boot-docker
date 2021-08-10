4953f6dad1d9cb92107159858ab4123bd99a001b
idsvnasia <74672963+idsvnasia@users.noreply.github.com>
2021-08-10 18:09:44 +0700

    Update entrypoint.bash

--- complete/entrypoint.bash
+++ complete/entrypoint.bash
@@ -8,15 +8,11 @@ TAG_NAME=v$BUILD_NUMBER
 
 #./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=springio/pulse-lead-agent:$TAG_NAME
 
-./mvnw package
-
-java -jar   app/app.jar
-
-ls -la ./target
 
 docker build -t springio/gs-spring-boot-docker:$TAG_NAME  .
+
 
 docker run -e "SPRING_PROFILES_ACTIVE=dev" -p 8080:8080 -t springio/gs-spring-boot-docker:$TAG_NAME
 
 
-docker run -p 8080:8080 -t springio/gs-spring-boot-docker:${TAG_NAME}
+#docker run -p 8080:8080 -t springio/gs-spring-boot-docker:${TAG_NAME}
